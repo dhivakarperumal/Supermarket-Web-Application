29fd6f02469827a706bc3b58d70f261281771dd0
qtechx001 <qtechx001@gmail.com>
2026-07-08 16:25:46 +0530

    display fix

--- Frontend/src/Admin/Staff/Staffs.jsx
+++ Frontend/src/Admin/Staff/Staffs.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import api from "../../api";
+import cache from "../../../cache";
 
 import {
   FaUsers,
